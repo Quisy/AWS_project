20190ed5a5bd0161a6227939323c3749809f0a06
Quisy <lamprecht.mariusz@gmail.com>
2018-01-28 18:49:37 +0100

    Added babel-polyfill for handling async/await

--- Client/api/server.js
+++ Client/api/server.js
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import PictureRoutes from './routes/pictureRoutes';
@@ -5,25 +7,9 @@ import fs from 'fs';
 import Helpers from './utils/helpers';
 import Service from './service';
 
+
+
 let port = process.env.PORT || 3000;
-// let ACTIONS_FOLDER = "./actions/";
-// let ACTIONS_CONFIG_FILE = "./api/configuration/actions.json";
-
-// let helpers = new Helpers();
-// let actionsCofig = helpers.readJSONFile(ACTIONS_CONFIG_FILE);
-
-// actionsCofig.forEach(function (elem) {
-//     if (elem.action && elem.path) {
-//         if (!elem.action.template) {
-//             elem.action = require(ACTIONS_FOLDER + elem.action).action;
-//             console.log(elem.action);
-//         }
-//     } else {
-//         console.log("unknown configuration: " + JSON.stringify(elem));
-//     }
-// });
-
-
 
 let app = express();
 app.use(bodyParser.urlencoded());
@@ -33,9 +19,5 @@ app.use(bodyParser.json());
 let pictureRouters = new PictureRoutes(app);
 
 app.listen(port);
-
-// let appService = new Service(actionsCofig);
-// appService.run(port);
-
 export default app
 
